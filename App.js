77c187ae64f57d7ce05b19ae4ebfcbb95d4c9470
MaxDevelo <maxime.wawrzy@gmail.com>
2024-09-30 18:34:21 +0200

    feat: install packages

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import database from '@react-native-firebase/database';
 
 export default function App() {
   return (
